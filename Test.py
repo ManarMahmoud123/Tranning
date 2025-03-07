8dbda38d1147f973e553d8cf732b40bdd673b24b
ManarMahmoud123 <manarmelshimey@gmail.com>
2025-03-07 06:51:55 +0200

    Update Test.py
    أضافه اسمى والرقم التعريفى

--- Test.py
+++ Test.py
@@ -24,6 +24,6 @@ id_name7 =2320155
 print("الاسم:" name7)
 print("ID:", id_number7name4 = "huda hamdy eldeb"
 name8 = "MANAR ELSHIMY"
-id_name6 =2320636
+id_name8 =2320636
 print("الاسم:" name8)
 print("ID:", id_number8)
